59dbff23a2cdd1210343f2fbcc383bec6e5b0c9b
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-07-25 04:23:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.java
@@ -0,0 +1,12 @@
+class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        // code here
+           return ((n >> k) & 1) == 1;
+}
+}
+
+
+
+
+        // Seedha bit nikal kar check karo ki wo 1 ke equal hai ya nahi
+    
