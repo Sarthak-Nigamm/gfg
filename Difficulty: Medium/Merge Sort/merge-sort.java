ef13418b4f1f850bbc8a5c417022b8b8f7c1ef40
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-10 19:11:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,35 +1,39 @@
 class Solution {
     public void merge(int arr[], int si, int ei,int mid) {
-      int temp[] = new int[ei-si+1];
-      int i =si,j=mid+1,k=0;
-      while(i<=mid && j<=ei){
-          if(arr[i]<=arr[j]){
-              temp[k] = arr[i];
-              i++;
-          }else{
-              temp[k] = arr[j];
-              j++;
-          }
-          k++;
-      }
-      while(i<=mid){
-          temp[k++] = arr[i++];
-      }
-       while(j<=ei){
-          temp[k++] = arr[j++];
-      }
-      //copy
-      for(int c = si; c<=ei;c++){
-          arr[c] = temp[c-si];
-      }
+        int temp[] = new int[ei-si+1];
+        int i =si,j=mid+1,k=0;
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            temp[k] = arr[i];
+                i++;
+                k++;
+        }
+        while(j<=ei){
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(int c =si; c<=ei; c++){
+            arr[c] = temp[c-si];
+        }
+ 
     }
     public void mergeSort(int arr[], int si, int ei) {
-       if(si>=ei){
-           return;
-       }
-        int mid = si+(ei-si)/2;
-        mergeSort(arr,si,mid);
-        mergeSort(arr,mid+1,ei);
-        merge(arr,si,ei,mid);
+     if(si>=ei){
+         return;
+     }
+     int mid = si + (ei-si)/2;
+     mergeSort(arr,si,mid);
+     mergeSort(arr,mid+1,ei);
+     merge(arr,si,ei,mid);
     }
 }
