dac89e7d297ce928c6da18e5c2c97192d8f9f38f
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-10 19:29:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKRotation(int arr[]) {
+        // Code here
+        int i =0, j = arr.length-1;
+        while(i<j){
+            int mid = i+(j-i)/2;
+            if(arr[mid]>arr[j]){
+                i = mid+1;
+            }else{
+                j = mid;
+            }
+        }
+   return i; }
+}
